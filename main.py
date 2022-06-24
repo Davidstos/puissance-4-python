57421482406d8096e9cc7db2887ad5509a24c622
David Dias <daviddias38@outlook.com>
2022-06-24 16:39:14 +0200

    add coin into grid

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ retry="o"
 # Grid List
 grid=[]
 row_count_column = []
+unvalid = True
 
 
 def count_column():
@@ -18,8 +19,6 @@ def count_column():
     for i in range(nb_column):
         row_count_column.append(i)
     return row_count_column
-for row in range(nb_row):
-    grid.append(["."]*nb_column)
 
 # Print grid
 def print_grid():
@@ -29,15 +28,42 @@ def print_grid():
         print()
     for char in count_column():
         print(char, end=' ')
+    print()
+
+def add_coin(choice, symbol):
+    for row in grid[::-1]:
+        if row[choice] == ".":
+            row[choice] = symbol
+            return False
+    return True
+    """
+    add_coin(int(input("Impossible rejouez :\n")), symbol)
+    We could also have used this recursion but to avoid certain nesting limits we 
+    have used an alternative
+    """
+
+for row in range(nb_row):
+    grid.append(["."]*nb_column)
 
 while retry == "o" or retry == "O":
     victory = False
+    print_grid()
     while victory==False:
+        while unvalid:
+            choice = int(input('Joueur 1 à vous de jouer!\n'))
+            unvalid = add_coin(choice, "o")
+            if unvalid:
+                print('Colonne pleine rejouez!\n')
+        unvalid = True
         print_grid()
-        print()
-        choice = input('Joueur 1 à vous de jouer!\n')
-        choice = input('Joueur 2 à vous de jouer!\n')
-        victory = True
+        while unvalid:
+            choice = int(input('Joueur 2 à vous de jouer!\n'))
+            unvalid = add_coin(choice, "x")
+            if unvalid:
+                print('Colonne pleine rejouez!\n')
+        unvalid = True
+        print_grid()
+        # victory = True
     retry = input("Voulez-vous rejouer? [O/N]")
 
 
